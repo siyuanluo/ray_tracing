1353cdc4f3087bb85c58cb15f7708b3e10ee84af
Siyuan Luo <3175211481@qq.com>
2021-02-02 19:37:12 +0800

    Update 2.2

---
+++ chap4/chap4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  chap4
+//
+//  Created by lsy on 02/02/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
